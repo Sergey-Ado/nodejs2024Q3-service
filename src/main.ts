a2d9cdfaca4f164a28399d29bd769d5a0ba4f257
Sergey-Ado <adonisiy2@gmail.com>
2024-11-02 20:37:28 +0400

    feat: add openApi

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
+import * as yaml from 'yamljs';
+import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const doc: OpenAPIObject = yaml.load('./doc/api.yaml');
+  SwaggerModule.setup('doc', app, doc);
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
   await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
