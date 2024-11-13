542da4341b7871755b2255588519b78951496785
Sergey-Ado <adonisiy2@gmail.com>
2024-11-13 20:00:51 +0400

    feat: add prisma to project

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,11 @@ import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { FavoriteModule } from './favorite/favorite.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   controllers: [AppController],
   providers: [AppService],
-  imports: [UserModule, ArtistModule, AlbumModule, TrackModule, FavoriteModule],
+  imports: [UserModule, ArtistModule, AlbumModule, TrackModule, FavoriteModule, PrismaModule],
 })
 export class AppModule {}
