c9f99523b6700de09b3aecce77e9232d1ffa1a92
Sergey-Ado <adonisiy2@gmail.com>
2024-11-24 21:32:41 +0400

    feat: add catch.everything.filter

--- src/logger/logger.middleware.ts
+++ src/logger/logger.middleware.ts
@@ -1,10 +1,11 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 import { LoggingService } from './logger.service';
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  constructor(private logger: LoggingService) {}
+  // constructor(private logger: LoggingService) {}
+  private logger = new Logger(LoggingService.name);
 
   use(req: Request, res: Response, next: NextFunction) {
     res.on('finish', () => {
@@ -14,8 +15,10 @@ export class LoggerMiddleware implements NestMiddleware {
         req.body,
       )} | STATUSCODE: ${res.statusCode}`;
 
-      if (res.statusCode < 400) this.logger.log(message, 'NestApplication');
-      else this.logger.error(message, 'NestApplication');
+      // if (res.statusCode < 400) this.logger.log(message, 'NestApplication');
+      // else this.logger.error(message, 'NestApplication');
+      if (res.statusCode < 400) this.logger.log(message);
+      else this.logger.error(message);
     });
     next();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import * as yaml from 'yamljs';
 import { OpenAPIObject, SwaggerModule } from '@nestjs/swagger';
 import { LoggingService } from './logger/logger.service';
+import { CatchEverythingFilter } from './filter/catch.everything.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -14,6 +15,9 @@ async function bootstrap() {
   const doc: OpenAPIObject = yaml.load('./doc/api.yaml');
   SwaggerModule.setup('doc', app, doc);
 
+  const logger = new LoggingService();
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new CatchEverythingFilter(httpAdapter));
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useLogger(app.get(LoggingService));
 
